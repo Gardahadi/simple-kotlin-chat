dd9e6fe42dfa9457a72f12c9cd350849c512bd40
Gardahadi <gardahadi@gardas-MacBook-Pro.local>
2020-04-24 18:36:41 +0700

    Fixed chat layout

--- app/src/main/java/com/example/simplechat/ui/main/ChatroomFragment.kt
+++ app/src/main/java/com/example/simplechat/ui/main/ChatroomFragment.kt
@@ -1,17 +1,14 @@
 package com.example.simplechat.ui.main
 
-import android.app.Activity
-import androidx.lifecycle.ViewModelProviders
+
 import android.os.Bundle
 import android.os.Handler
 import android.text.Html
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
-import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.EditorInfo
-import android.widget.Adapter
 import android.widget.ProgressBar
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
